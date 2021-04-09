6d428d49a124724372155b0331bd24eb6082bb7d
relesi <renatolessa.2011@hotmail.com>
2021-04-09 16:30:19 -0400

    implementation to service rest

---
+++ src/main/kotlin/br/com/relesi/cloud/services/provider/ProviderApplication.kt
@@ -0,0 +1,11 @@
+package br.com.relesi.cloud.services.provider
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProviderApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProviderApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/relesi/cloud/services/provider/ProviderApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.relesi.cloud.services.provider
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProviderApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
