d571bfc636b1d6f823faba7cf2aebf6d60f57579
relesi <renatolessa.2011@hotmail.com>
2021-05-03 09:48:28 -0300

    implement token to certain service or endpoint

--- src/main/java/br/com/relesi/cloud/services/provider/ResourceServerConfigurer.java
+++ src/main/java/br/com/relesi/cloud/services/provider/ResourceServerConfigurer.java
@@ -1,6 +1,7 @@
 package br.com.relesi.cloud.services.provider;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
@@ -10,7 +11,7 @@ public class ResourceServerConfigurer extends ResourceServerConfigurerAdapter {
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-		.anyRequest()
-		.authenticated();
+		.antMatchers(HttpMethod.POST, "/order")
+		.hasRole("USER");
 	}
 }
